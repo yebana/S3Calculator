084ab9ee95487b1658b1e2d46f53d2913eeed354
yebana <yebana@outlook.com>
2025-04-04 09:02:10 +0000

    modified text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,20 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to AWS S3 Calculator! 👋")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select an option above.")
 
 st.markdown(
     """
     Streamlit is an open-source app framework built specifically for
     Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
+    **👈 Select an option from the sidebar** to calculate some 
+    services involved in a AWS S3 Environment !
     ### Want to learn more?
     - Check out [streamlit.io](https://streamlit.io)
     - Jump into our [documentation](https://docs.streamlit.io)
     - Ask a question in our [community
         forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
